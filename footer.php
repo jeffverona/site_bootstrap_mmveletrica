ab4e4e6a1e98066c786303d7786dbeef418dde57
Jefferson Verona <jefferson.verona@gmail.com>
2017-06-03 17:30:53 -0300

    mudando header e footer para php

---
+++ footer.php
@@ -0,0 +1,71 @@
+	<!-- Footer -->
+	<div class="page-footer">
+		<div class="back-to-top hidden-xs"><a href="#top"><span class="icon icon-lightning"></span></a></div>
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">Serviço de Emergência 24horas</div>
+					<div class="col-sm-1 hidden-xs">&nbsp;</div>
+					<div class="col-sm-3">Orçamento Grátis</div>
+					<div class="col-sm-1 hidden-xs">&nbsp;</div>
+					<div class="col-sm-3">(11) 97371-2252</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row footer-row">
+				<div class="col-sm-3 col-lg-3">
+					<div class="logo">
+						<a href="index.html"><img src="images/logo-invert.png" alt="Logo"></a>
+					</div>
+					<div class="hidden-xs">
+						<div class="copyright">© 2017 MMV Eletica.
+							<br> Todos os direitos reservados.</div>
+						<div class="social-links">
+							<ul>
+								<li>
+									<a class="icon icon-facebook" href="#"></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<ul class="contact-list">
+						<li><span class="list-label"><b>T</b>elefone:  </span> <span class="text">(11) 97371-2252</span></li>
+						<li><span class="list-label"><b>E</b>-mail:</span> <span class="text"><a href="mailto:info@yourdomain.com">info@yourdomain.com</a></span></li>
+						<li><span class="list-label"><b>E</b>ndereço:</span> <span class="text">Rua Bugária, Nº 97 - Jd. Andriane - Itaquaquecetuba</span></li>
+						<li>De Segunda á Sexta das <b>08:00</b> ás <b>17:30</b></li>
+					</ul>
+				</div>
+				<div class="col-sm-5">
+					<div id="footer-map"></div>
+				</div>
+				<div class="visible-xs text-center">
+					<div class="copyright">© 2017 MMV Elétrica.
+						<br> Todos os direitos reservados</div>
+					<div class="social-links">
+						<ul>
+							<li>
+								<a class="icon icon-facebook" href="#"></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //Footer -->
+
+	<script src="js/jquery.js"></script>
+	<script src="js/plugins/bootstrap.min.js"></script>
+	<script src="js/plugins/jquery.nivo.slider.js"></script>
+	<script src="js/plugins/slick.min.js"></script>
+	<script src="js/plugins/jquery.magnific-popup.min.js"></script>
+	<script src="js/plugins/imagesloaded.pkgd.min.js"></script>
+	<script src="js/plugins/isotope.pkgd.min.js"></script>
+	<script src="js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="html 5 template">
+	<meta name="author" content="tonytemplates.com">
+	<link rel="icon" href="favicon.ico">
+	<title>Electrician - HTML 5 TEMPLATE</title>
+	<!-- Bootstrap core CSS -->
+	<link href="css/plugins/bootstrap.min.css" rel="stylesheet">
+	<link href="css/plugins/bootstrap-submenu.css" rel="stylesheet">
+	<link href="css/plugins/animate.min.css" rel="stylesheet">
+	<link href="css/plugins/nivo-slider.css" rel="stylesheet">
+	<link href="css/plugins/slick.css" rel="stylesheet">
+	<link href="css/plugins/magnific-popup.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<!-- Icon Font-->
+	<link href="iconfont/style.css" rel="stylesheet">
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,400,500,700,900" rel="stylesheet">
+	<!-- Google map -->
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+</head>
+
+<body class="home">
+	<!-- Loader -->
+	<div id="loader-wrapper" class="loader-on">
+		<div id="loader">
+			<div class="loader">
+				<div class="bolt one">
+					<div class="other"></div>
+				</div>
+				<div class="bolt two">
+					<div class="other"></div>
+				</div>
+				<div class="bolt three">
+					<div class="other"></div>
+				</div>Carregando
+			</div>
+		</div>
+	</div>
+	<!-- //Loader -->
+	<!-- Header -->
+	<header class="page-header">
+		<!-- Fixed navbar -->
+		<nav class="navbar" id="slide-nav">
+			<div class="container">
+				<div class="navbar-header">
+					<div class="header-top">
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="social-links">
+									<ul>
+										<!--<li>
+											<a class="icon icon-twitter" href="#"></a>
+										</li>
+										<li>
+											<a class="icon icon-instagram" href="#"></a>
+										</li>-->
+										<li>
+											<a class="icon icon-facebook" href="#"></a>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-sm-4 text-center">
+								<div class="logo">
+									<a href="index.html"><img src="images/logo.png" alt="Logo">
+										<span></span></a>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="phone">
+									<div class="number"><i class="icon icon-telephone"></i><span>(11) 97371-2252</span></div>
+									<div class="under-number">Ligue Agora, e solicite um orçamento!</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<button type="button" class="navbar-toggle"><i class="icon icon-interface icon-menu"></i><i class="icon icon-cancel"></i></button>
+				</div>
+				<div id="slidemenu" data-hover="dropdown" data-animations="fadeIn">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.html"><span class="electric-btn"><span class="text">Home</span></span></a></li>
+						<li><a href="services.html"><span class="electric-btn"><span class="text">Serviços</span></span></a></li>
+						<li><a href="gallery.html"><span class="electric-btn"><span class="text">Galeria</span></span></a></li>
+						<li><a href="contact.html"><span class="electric-btn"><span class="text">Contatos</span></span></a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- // Header -->
